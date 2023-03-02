be205d8165a98825454c218a91fb2510b13278c1
LHNFPV <77920881+LHNFPV@users.noreply.github.com>
2023-03-02 19:13:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oD33m6EjyXNZtooVXNoG7RwIq848',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pbdlDIhQ0kZcDJCARKbgu67UUzuCE1SJiv7OQrtVDns	',
+      useTemplateId: 'OniD6SyfI136hRgWB9Gd9IlwPTtz8mq00P61bi9GCg4	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-04',
       festivals: [
